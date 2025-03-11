d7c6b0bff792d15b7ed4eba7433980d446497fde
rifqifarelmuhammad <rifqifarel222@gmail.com>
2025-03-11 11:42:11 +0700

    chore: bootstrapped the repository

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { ValidationPipe, Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule)
+    app.useGlobalPipes(new ValidationPipe())
+    app.enableCors({
+        origin: process.env.APP_ALLOWED_ORIGIN,
+        credentials: true,
+    })
+
+    await app.listen(process.env.APP_PORT ?? 3000)
+    Logger.log(`Listening on ${await app.getUrl()}`)
+}
+
+bootstrap()
