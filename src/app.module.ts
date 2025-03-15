b63156b3b9d94722b438c206baa8895b9c32a14b
rifqifarelmuhammad <rifqifarel222@gmail.com>
2025-03-16 02:36:02 +0700

    chore: added some classes and helper methods related to authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,25 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
-// import { PrismaModule } from './prisma/prisma.module'
+import { APP_FILTER } from '@nestjs/core'
+import { PrismaModule } from './prisma/prisma.module'
+import { CommonsModule } from './commons/commons.module'
+import { HttpExceptionFilter } from './commons/filters/http-exception.filter'
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         JwtModule.register({}),
-        // PrismaModule,
+        CommonsModule,
+        PrismaModule,
     ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        AppService,
+        {
+            provide: APP_FILTER,
+            useClass: HttpExceptionFilter,
+        }
+    ],
 })
 export class AppModule {}
