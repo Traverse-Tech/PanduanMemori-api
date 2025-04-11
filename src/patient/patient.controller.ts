3c976212a15536115ca56222eaa03c27421be958
ghaydarafa <ghayda.rafa@gmail.com>
2025-04-12 06:19:55 +0700

    fix: getPatientCaregivers

--- src/patient/patient.controller.ts
+++ src/patient/patient.controller.ts
@@ -42,8 +42,9 @@ export class PatientController {
 
     @IsPatient()
     @Get('caregivers')
-    async getCaregivers(@GetCurrentUser() user: User) {
-        return this.patientService.getCaregivers(user)
+    async getPatientCaregivers(@GetCurrentUser() user: User) {
+        const responseData = await this.patientService.getPatientCaregivers(user)
+        return this.responseUtil.response({}, responseData)
     }
 
     @IsPatient()
